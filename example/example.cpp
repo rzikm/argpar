aefedf5cdce591535a38dde97e329d819afe4b51
Radek Zikmund <r.zikmund.rz@gmail.com>
2019-03-24 17:42:47 +0100

    Finish the mixin code

--- example/example.cpp
+++ example/example.cpp
@@ -5,7 +5,7 @@
 #include <optional>
 #include <memory>
 
-int main(int argc, char* argv[])
+int main(int argc, char * argv[])
 {
 	bool has_version;
 	std::string format;
@@ -16,40 +16,63 @@ int main(int argc, char* argv[])
 	bool has_help;
 
 	argpar::parser parser;
-	parser.option({ "V", "version" }, "Prints out version and exits successfully", &has_version); // optional option
-	parser.option({ "f", "format" }, "Sets format for the output.") // mandatory option
-		.string_val("FORMAT", &format); // mandatory parameter
-	parser.option({ "v", "verbose" }, "Enables verbose output.", &has_verbose);
-	parser.option({ "help" }, "Prints out usage and exits successfully", &has_help);
+	parser.option({"V", "version"}, "Prints out version and exits successfully", &has_version); // optional option
+	parser.option({"f", "format"}, "Sets format for the output.") // mandatory option
+	      .string_val("FORMAT", &format); // mandatory parameter
+	parser.option({"v", "verbose"}, "Enables verbose output.", &has_verbose);
+	parser.option({"help"}, "Prints out usage and exits successfully", &has_help);
 	parser.argument().string_val("command", &command);
 	parser.argument_list().string_val("arguments", &commandArgs);
 
 	// other use cases
 	bool do_magic;
 	int magic_level;
-	parser.option({ "optional-option-with-optional-parameter" }, "Does something mega useful.", &do_magic)
-		.int_val("MAGIC_LEVEL", &magic_level).between(1, 8).with_default(5); // optional parameter
+	parser.option({"optional-option-with-optional-parameter"}, "Does something mega useful.", &do_magic)
+	      .int_val("MAGIC_LEVEL", &magic_level).between(1, 8).with_default(5); // optional parameter
 
-	class custom_config : public argpar::defaultable<custom_config>
+	struct coords
 	{
-	public:
-		using value_type = std::unique_ptr<int>;
-
-		value_type parse(char const * arg) const
-		{
-			return std::make_unique<int>(1);
-		}
+		float x;
+		float y;
 	};
 
-	std::unique_ptr<int> my_ptr;
+	class custom_config : public argpar::cfg_base<custom_config, coords>
+	{
+	public:
+		// constrains the parameter value to certain circular area
+		custom_config & from_area(coords center, float radius)
+		{
+			has_area_restriction_ = true;
+			area_center_ = center;
+			area_radius_ = radius;
 
-	parser.argument().custom_val<custom_config>("myCustomConf", &my_ptr);
+			return *this;
+		}
+
+		// value_type typedef is inherited from base_cfg
+		value_type parse(char const * arg) const
+		{
+			coords result = {0, 0};
+			// parse values from arg
+			// check if satisfies constrains, possibly throw argpar::format_error.
+			return result;
+		}
+
+	private:
+		bool has_area_restriction_ = false;
+		coords area_center_ = {};
+		float area_radius_ = {};
+	};
+
+	coords my_coords;
+	parser.argument().custom_val<custom_config>("myCustomConf", &my_coords)
+	      .from_area({0, 0}, 20).with_default(coords{10, 0});
 
 	try
 	{
 		parser.parse(argc, argv);
 	}
-	catch (argpar::parse_error& e)
+	catch (argpar::parse_error & e)
 	{
 		std::cout << e.what() << std::endl;
 		return -1;
--- src/argpar.cpp
+++ src/argpar.cpp
@@ -9,91 +9,91 @@ static double_cfg dbl_config_;
 static value_config value_config_;
 static value_list_config value_list_config_;
 
-string_cfg& string_cfg::from(std::vector<std::string> const& values)
+string_cfg & string_cfg::from(std::vector<std::string> const & values)
 {
 	return *this;
 }
 
-string_cfg& string_cfg::with_default(std::string const& value)
+string_cfg & string_cfg::with_default(std::string const & value)
 {
 	return *this;
 }
 
-int_cfg& int_cfg::between(int min, int max)
+int_cfg & int_cfg::between(int min, int max)
 {
 	return *this;
 }
 
-int_cfg& int_cfg::with_default(int value)
+int_cfg & int_cfg::with_default(int value)
 {
 	return *this;
 }
 
-double_cfg& double_cfg::between(double min, double max)
+double_cfg & double_cfg::between(double min, double max)
 {
 	return *this;
 }
 
-double_cfg& double_cfg::with_default(double value)
+double_cfg & double_cfg::with_default(double value)
 {
 	return *this;
 }
 
-int_cfg& value_config::int_val(std::string const& name, int* par_dest)
+int_cfg & value_config::int_val(std::string const & name, int * par_dest)
 {
 	return int_config_;
 }
 
-string_cfg& value_config::string_val(std::string const& name, std::string* par_dest)
+string_cfg & value_config::string_val(std::string const & name, std::string * par_dest)
 {
 	return str_config_;
 }
 
-double_cfg& value_config::double_val(std::string const& name, double* par_dest)
+double_cfg & value_config::double_val(std::string const & name, double * par_dest)
 {
 	return dbl_config_;
 }
 
-int_cfg& value_list_config::int_val(std::string const& name, std::vector<int>* par_dest)
+int_cfg & value_list_config::int_val(std::string const & name, std::vector<int> * par_dest)
 {
 	return int_config_;
 }
 
-string_cfg& value_list_config::string_val(std::string const& name, std::vector<std::string>* par_dest)
+string_cfg & value_list_config::string_val(std::string const & name, std::vector<std::string> * par_dest)
 {
 	return str_config_;
 }
 
-double_cfg& value_list_config::double_val(std::string const& name, std::vector<double>* par_dest)
+double_cfg & value_list_config::double_val(std::string const & name, std::vector<double> * par_dest)
 {
 	return dbl_config_;
 }
 
-value_config& parser::option(std::vector<std::string> const& aliases, std::string const& hint)
+value_config & parser::option(std::vector<std::string> const & aliases, std::string const & hint)
 {
 	return value_config_;
 }
 
-value_config& parser::option(std::vector<std::string> const& aliases, std::string const& hint,
-                             bool* opt_dest)
+value_config & parser::option(std::vector<std::string> const & aliases, std::string const & hint,
+                              bool * opt_dest)
 {
 	return value_config_;
 }
 
-value_config& parser::argument()
+value_config & parser::argument()
 {
 	return value_config_;
 }
 
-value_list_config& parser::argument_list()
+value_list_config & parser::argument_list()
 {
 	return value_list_config_;
 }
 
-void parser::parse(int p_argc, char** p_argv)
+void parser::parse(int p_argc, char ** p_argv)
 {
 }
 
-void parser::print_help(std::ostream& stream)
+void parser::print_help(std::ostream & stream)
 {
 }
