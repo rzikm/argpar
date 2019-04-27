198af26ebe6f98db7e00e3a88592e65050ed2206
Radek Zikmund <r.zikmund.rz@gmail.com>
2019-04-27 17:26:36 +0200

    Converted library to header-only

--- include/argpar/argpar.h
+++ include/argpar/argpar.h
@@ -253,7 +253,13 @@ public:
 	 * \param[in] values Allowed set of values.
 	 * \return Returns reference to this object for method chaining.
 	 */
-	string_cfg & from(std::vector<std::string> const & values);
+	string_cfg & from(std::vector<std::string> const & values)
+	{
+		if (values.empty()) throw std::invalid_argument("Values array must be nonempty");
+		values_.resize(values.size());
+		std::copy(values.begin(), values.end(), values_.begin());
+		return *this;
+	}
 
 	std::string parse(std::string const & value) const
 	{
@@ -434,7 +440,10 @@ public:
 	 * pointer must be valid during the associated parse() call.
 	 * \return Returns object for further configuration of the parameter.
 	 */
-	int_cfg & int_val(std::string const & name, std::vector<int> * dest);
+	int_cfg & int_val(std::string const & name, std::vector<int> * dest)
+	{
+		return custom_val<int_cfg>(name, dest);
+	}
 
 	/**
 	 * Configures the list to contain string arguments.
@@ -444,7 +453,10 @@ public:
 	 * pointer must be valid during the associated parse() call.
 	 * \return Returns object for further configuration of the parameter.
 	 */
-	string_cfg & string_val(std::string const & name, std::vector<std::string> * dest);
+	string_cfg & string_val(std::string const & name, std::vector<std::string> * dest)
+	{
+		return custom_val<string_cfg>(name, dest);
+	}
 
 	/**
 	 * Configures the list to accept a floating point number arguments.
@@ -454,7 +466,10 @@ public:
 	 * pointer must be valid during the associated parse() call.
 	 * \return Returns object for further configuration of the parameter.
 	 */
-	double_cfg & double_val(std::string const & name, std::vector<double> * dest);
+	double_cfg & double_val(std::string const & name, std::vector<double> * dest)
+	{
+		return custom_val<double_cfg>(name, dest);
+	}
 
 	/**
 	 * Configures the option or argument to accept a parameter of custom type.
@@ -508,7 +523,10 @@ public:
 	 * pointer must be valid during the associated parse() call.
 	 * \return Returns object for further configuration of the parameter.
 	 */
-	int_cfg & int_val(std::string const & name, int * dest);
+	int_cfg & int_val(std::string const & name, int * dest)
+	{
+		return custom_val<int_cfg>(name, dest);
+	}
 
 	/**
 	 * Configures the option or argument to accept a string parameter.
@@ -518,7 +536,10 @@ public:
 	 * pointer must be valid during the associated parse() call.
 	 * \return Returns object for further configuration of the parameter.
 	 */
-	string_cfg & string_val(std::string const & name, std::string * dest);
+	string_cfg & string_val(std::string const & name, std::string * dest)
+	{
+		return custom_val<string_cfg>(name, dest);
+	}
 
 	/**
 	 * Configures the option or argument to accept a floating point number parameter.
@@ -528,7 +549,10 @@ public:
 	 * pointer must be valid during the associated parse() call.
 	 * \return Returns object for further configuration of the parameter.
 	 */
-	double_cfg & double_val(std::string const & name, double * dest);
+	double_cfg & double_val(std::string const & name, double * dest)
+	{
+		return custom_val<double_cfg>(name, dest);
+	}
 
 	/**
 	 * Configures the option or argument to accept a parameter of custom type.
@@ -581,11 +605,29 @@ public:
 	};
 
 	option(std::vector<std::string> const & aliases, std::string const & hint, bool * flag_dest)
-		: aliases_(aliases)
+		: short_name_(0)
+		, long_name_("")
 		, hint_(hint)
 		, flag_dest_(flag_dest)
 		, found_(false)
 	{
+		if (aliases.size() > 2) throw std::invalid_argument("Too many aliases for option");
+		if (aliases.empty()) throw std::invalid_argument("Set of aliases cannot be empty");
+
+		for (auto && alias : aliases)
+		{
+			if (alias.empty()) throw std::invalid_argument("Alias cannot be zero length.");
+			if (alias.size() == 1)
+			{
+				if (short_name_ != 0) throw std::invalid_argument("Only one short name can be given for an option.");
+				short_name_ = alias[0];
+			}
+			else
+			{
+				if (!long_name_.empty()) throw std::invalid_argument("Only one long name can be given for an option.");
+				long_name_ = std::move(alias);
+			}
+		}
 	}
 
 	value_config & value_cfg()
@@ -606,9 +648,14 @@ public:
 			: arg_type::mandatory;
 	}
 
-	std::vector<std::string> const & aliases() const
+	char short_name() const
 	{
-		return aliases_;
+		return short_name_;
+	}
+
+	std::string const & long_name() const
+	{
+		return long_name_;
 	}
 
 	std::string const & hint() const
@@ -628,7 +675,8 @@ public:
 		return found_;
 	}
 private:
-	std::vector<std::string> aliases_;
+	char short_name_;
+	std::string long_name_;
 	std::string hint_;
 	bool * flag_dest_;
 	value_config value_config_;
@@ -650,7 +698,10 @@ public:
 	 * another option or some alias contains forbidden characters.
 	 * \return Reference to an object which can be used to further configure the option.
 	 */
-	value_config & option(std::vector<std::string> const & aliases, std::string const & hint);
+	value_config & option(std::vector<std::string> const & aliases, std::string const & hint)
+	{
+		return option_unchecked(aliases, hint, nullptr);
+	}
 
 	/**
 	 * Defines a new optional option.
@@ -663,14 +714,23 @@ public:
 	 * another option or some alias contains forbidden characters.
 	 * \return Reference to an object which can be used to further configure the option.
 	 */
-	value_config & option(std::vector<std::string> const & aliases, std::string const & hint, bool * flag_dest);
+	value_config & option(std::vector<std::string> const & aliases, std::string const & hint, bool * flag_dest)
+	{
+		if (!flag_dest) throw std::invalid_argument("Argument opt_dest cannot be nullptr");
+		return option_unchecked(aliases, hint, flag_dest);
+	}
 
 	/**
 	 * Defines a new positional argument. Use method chaining to further configure it's type.
 	 * \throw std::logic_error if argument_list() method has been called before.
 	 * \return Reference to an object which can be used to cofigure the value of the argument.
 	 */
-	value_config & argument();
+	value_config & argument()
+	{
+		if (additional_arguments_.has_value()) throw std::logic_error("Positional arguments cannot be defined after argument_list() was called");
+		// if (positional_arguments_.size() > 0 && positional_arguments_[positional_arguments_.size()]->)
+		return *positional_arguments_.emplace_back(std::make_unique<value_config>());
+	}
 
 	/**
 	 * Defines a variable number of positional arguments of the same type. No more arguments can be
@@ -678,8 +738,44 @@ public:
 	 * \throw std::logic_error if argument_list() method has already been called before.
 	 * \return Reference to an object which can be used to cofigure the value of the argument.
 	 */
-	value_list_config & argument_list();
-	void set_parsed_value(argpar::option *& current_option, std::string option_name, std::optional<std::string> value);
+	value_list_config & argument_list()
+	{
+		if (additional_arguments_.has_value()) throw std::logic_error("Function argument_list() was already called");
+		return additional_arguments_.emplace();
+	}
+
+	void set_parsed_value(argpar::option *& current_option, std::string option_name, std::optional<std::string> value)
+	{
+		try
+		{
+			switch (current_option->arg_type())
+			{
+			case option::arg_type::no_arg:
+				if (value.has_value())
+					throw argpar::bad_value(option_name, value.value(), make_str("Option '", option_name, "' does not take any values"));
+				current_option = nullptr; // parsing finished 
+				break;
+			case option::arg_type::optional:
+				if (value.has_value())
+					current_option->value_cfg().handler_->parse(value.value());
+				else
+					current_option->value_cfg().handler_->set_default();
+				current_option = nullptr; // parsing finished either way
+				break;
+			case option::arg_type::mandatory:
+				if (value.has_value())
+				{
+					current_option->value_cfg().handler_->parse(value.value());
+					current_option = nullptr; // parsing finished 
+				}
+				break;
+			}
+		}
+		catch (argpar::format_error& e)
+		{
+			throw argpar::bad_value(option_name, value.value(), e.message());
+		}
+	}
 
 	/**
 	 * Parses the options from the given command line arguments.
@@ -696,16 +792,179 @@ public:
 	 * \throw argpar::missing_value  if a mandatory parameter to an option or a mandatory positional
 	 *                               argument is not present.
 	 */
-	void parse(int argc, char ** argv);
-	void print_usage_line(std::ostream & stream);
+	void parse(int argc, char ** argv)
+	{
+		if (argc < 1) throw std::invalid_argument("Parameter argc cannot be less than 1.");
+		if (!argv) throw std::invalid_argument("Parameter argv cannot be nullptr.");
+		// TODO: Check validity
+
+		size_t i = 1;
+		std::vector<std::string> args(argv, argv + argc);
+
+		argpar::option * current_option = nullptr; // currently processed option
+		std::string option_name;
+		std::optional<std::string> value;
+		for (; i < args.size(); ++i)
+		{
+			value.reset();
+			std::string & arg = args[i];
+
+			if (arg == "--") // positional argument delimiter
+			{
+				++i;
+				break;
+			}
+
+			if (current_option) // still processing some option
+			{
+				set_parsed_value(current_option, option_name, arg);
+				continue;
+			}
+
+			if (arg.size() > 1 && arg[0] == '-')
+			{
+				if (arg[1] == '-') // long option
+				{
+					size_t eq_pos = arg.find('=');
+					if (eq_pos != std::string::npos)
+					{
+						// option_name = arg.substr(2, eq_pos - 2);
+						value = arg.substr(eq_pos + 1);
+					}
+					option_name = arg.substr(2, eq_pos - 2);
+					// option_name = arg.substr(2);
+					current_option = find_option(option_name);
+				}
+				else // short option
+				{
+					// set all condensed flags
+					size_t flag_pos = 0;
+					while (flag_pos < arg.size() - 1)
+					{
+						current_option = find_option(arg[++flag_pos]);
+						if (current_option->arg_type() != option::arg_type::no_arg) break;
+						current_option->set_found();
+					}
+					option_name = std::string(1, arg[flag_pos]);
+				}
+
+				current_option->set_found();
+				set_parsed_value(current_option, option_name, value);
+			}
+			else
+			{
+				// first positional arguments encountered
+				break;
+			}
+		}
+
+		if (current_option)
+			throw argpar::missing_value(option_name);
+
+		verify_options();
+
+		// process positional args
+		size_t pos = 0;
+		bool optional_only = false;
+		for (; i < args.size(); ++i, ++pos)
+		{
+			std::string & arg = args[i];
+			if (pos < positional_arguments_.size())
+			{
+				value_config & config = *positional_arguments_[pos];
+				config.handler_->parse(arg);
+			}
+			else
+			{
+				if (!additional_arguments_.has_value())
+					throw std::logic_error("Too many arguments.");
+				additional_arguments_->handler_->parse(arg);
+			}
+		}
+	}
+
+	void print_usage_line(std::ostream & stream)
+	{
+		stream << "Usage: ";
+		if (!options_.empty())
+		{
+			stream << "[OPTIONS...] ";
+		}
+		// for (auto && opt : options_)
+		// {
+			// if (opt->mandatory())
+			// {
+				// stream << ;
+			// }
+		// }
+		for (auto && arg : positional_arguments_)
+		{
+			stream << "[" << arg->handler_->name() << "] ";
+		}
+		if (additional_arguments_.has_value())
+		{
+			stream << "[" << additional_arguments_.value().handler_->name() << "...] ";
+		}
+		stream << "\n";
+	}
 
 	/**
 	 * Prints help clause describing usage of all registered options and parameters in a readable format.
 	 * \param[out] stream Stream to be written into.
 	 */
-	void print_help(std::ostream & stream);
+	void print_help(std::ostream & stream) {
+		print_usage_line(stream);
+
+		if (!options_.empty())
+		{
+			stream << "Options: \n";
+			for (auto && opt : options_)
+			{
+				stream << "\t";
+				bool first = true;
+				if (opt->short_name())
+				{
+					stream << "-" << opt->short_name() << ", ";
+				}
+				else
+				{
+					stream << "    ";
+				}
+
+				if (!opt->long_name().empty())
+				{
+					stream << "--" << opt->long_name() << " ";
+				}
+				if (opt->value_cfg().handler_)
+				{
+					// adequate set of parentheses
+					const char * parens = opt->value_cfg().handler_->has_default()
+						? "[]"
+						: "<>";
+					auto && handler = opt->value_cfg().handler_;
+					stream << parens[0] << handler->name();
+					if (handler->has_default())
+					{
+						stream << " = "; handler->print_default(stream);
+					}
+					stream << parens[1];
+				}
+				stream << "\n\t\t" << opt->hint();
+
+				stream << "\n\n";
+			}
+		}
+	}
 
 private:
+	argpar::value_config & option_unchecked(std::vector<std::string> const & aliases, std::string const & hint, bool * dest)
+	{
+		std::unique_ptr<argpar::option> option = std::make_unique<argpar::option>(aliases, hint, dest);
+		add_aliases(option.get());
+		options_.emplace_back(std::move(option));
+		return options_.back()->value_cfg();
+	}
+
 	argpar::option * find_option(std::string const & name)
 	{
 		auto it = long_to_option_.find(name);
@@ -733,11 +992,24 @@ private:
 		for (auto && option : options_)
 		{
 			if (option->mandatory() && !option->found())
-				throw argpar::missing_option(option->aliases()[0]);
+				throw argpar::missing_option(option->short_name() ? make_str(option->short_name()) : option->long_name());
 		}
 	}
 
-	void add_aliases(std::vector<std::string> const & aliases, argpar::option * option);
+	void add_aliases(argpar::option * opt)
+	{
+		// check for duplicate aliases
+		if (opt->short_name())
+		{
+			auto[_, inserted] = short_to_option_.try_emplace(opt->short_name(), opt);
+			if (!inserted) throw std::invalid_argument(make_str("Duplicate alias definition: ", opt->short_name(), "."));
+		}
+		if (!opt->long_name().empty())
+		{
+			auto[_, inserted] = long_to_option_.try_emplace(opt->long_name(), opt);
+			if (!inserted) throw std::invalid_argument(make_str("Duplicate alias definition: ", opt->long_name(), "."));
+		}
+	}
 
 	std::vector<std::unique_ptr<argpar::option>> options_;
 	std::unordered_map<std::string, argpar::option *> long_to_option_;
